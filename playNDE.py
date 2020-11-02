ac2456c57171652f7f249bf0f796c657c07e05ab
Scott <skchu@ualberta.ca>
2020-11-02 14:41:35 -0700

    making board init a func

---
+++ playNDE.py
@@ -0,0 +1,4 @@
+import setup
+
+board = setup.init()
+print(board)
--- setup.py
+++ setup.py
@@ -4,25 +4,27 @@ Intitalizing board and pieces
 import classes
 import sys
 
-board = classes.Board()
 
-red1 = classes.Piece(0, 0, "red", 1)
-red2 = classes.Piece(0, 1, "red", 2)
-red3 = classes.Piece(0, 2, "red", 3)
-red4 = classes.Piece(1, 0, "red", 4)
-red5 = classes.Piece(1, 1, "red", 5)
-red6 = classes.Piece(2, 0, "red", 6)
-blue1 = classes.Piece(4, 4, "blue", 1)
-blue2 = classes.Piece(4, 3, "blue", 2)
-blue3 = classes.Piece(4, 2, "blue", 3)
-blue4 = classes.Piece(3, 3, "blue", 4)
-blue5 = classes.Piece(3, 4, "blue", 5)
-blue6 = classes.Piece(2, 4, "blue", 6)
+def init():
+    board = classes.Board()
 
-pieceList = [red1, red2, red3, red4, red5, red6,
-             blue1, blue2, blue3, blue4, blue5, blue6]
+    red1 = classes.Piece(0, 0, "red", 1)
+    red2 = classes.Piece(0, 1, "red", 2)
+    red3 = classes.Piece(0, 2, "red", 3)
+    red4 = classes.Piece(1, 0, "red", 4)
+    red5 = classes.Piece(1, 1, "red", 5)
+    red6 = classes.Piece(2, 0, "red", 6)
+    blue1 = classes.Piece(4, 4, "blue", 1)
+    blue2 = classes.Piece(4, 3, "blue", 2)
+    blue3 = classes.Piece(4, 2, "blue", 3)
+    blue4 = classes.Piece(3, 3, "blue", 4)
+    blue5 = classes.Piece(3, 4, "blue", 5)
+    blue6 = classes.Piece(2, 4, "blue", 6)
 
-for piece in pieceList:
-    board.addPiece(piece)
+    pieceList = [red1, red2, red3, red4, red5, red6,
+                 blue1, blue2, blue3, blue4, blue5, blue6]
 
-print(board)
+    for piece in pieceList:
+        board.addPiece(piece)
+
+    return board
