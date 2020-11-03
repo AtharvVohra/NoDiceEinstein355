70cac39816982b5c60d4dda4ca88cf769210e3c2
Scott <skchu@ualberta.ca>
2020-11-02 23:10:03 -0700

    starting terminal ui

--- playNDE.py
+++ playNDE.py
@@ -1,4 +1,29 @@
 import setup
+import sys
 
-board = setup.init()
-print(board)
+
+def play():
+    board = setup.init()
+    # print(board)
+    blueToPlay = True
+
+    while True:
+        """
+        Check if game is over
+        """
+        redCorner = board.board[0][0]
+        blueCorner = board.board[4][4]
+        if redCorner and blueCorner:
+            if redCorner.color == "blue" and blueCorner.color == "red":
+                break
+
+    if blueToPlay:
+        pass
+    elif not blueToPlay:
+        pass
+
+    board.print_board()
+
+
+if __name__ == "__main__":
+    play()
